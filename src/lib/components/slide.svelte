06f8eec17846c34d6afe6f6fbc35ce7ae1e85737
pux <nick-ng@users.noreply.github.com>
2026-02-15 11:11:37 +1300

    feat: slide scale is determined automatically based on slide width

--- src/lib/components/slide.svelte
+++ src/lib/components/slide.svelte
@@ -1,30 +1,84 @@
 <script lang="ts">
+	import { sleep } from '$lib/utils';
 	import MarkdownIt from 'markdown-it';
+	import { onMount } from 'svelte';
 
 	interface Props {
 		slideId?: string | number;
-		topGap?: number;
-		fontSizePercent?: number;
 		slideMd: string;
 	}
 
-	let { slideId, slideMd, fontSizePercent = 180, topGap = 0 }: Props = $props();
-	let fontSizeStyle = $derived(`font-size: ${fontSizePercent}%;`);
-	let titleSlideStyle = $derived(slideId === 0 ? 'padding-top: 25vh; text-align: center;' : '');
+	const BASE_FONT_SIZE_PERCENT = 200;
+	const BASE_WIDTH_PX = 1920;
+	const BASE_HEIGHT_PX = 1080;
+
+	let { slideId, slideMd }: Props = $props();
+	let thisEl: HTMLDivElement | undefined = $state();
+	let actualFontSizePercent = $state(BASE_FONT_SIZE_PERCENT);
+	let paddingTopPx = $state(BASE_HEIGHT_PX / 3);
+	let shrink = $state(0);
+	let fontSizeStyle = $derived(`font-size: ${actualFontSizePercent}%;`);
+	let titleSlideStyle = $derived(
+		slideId === 0 ? `padding-top: ${paddingTopPx}px; text-align: center;` : ''
+	);
 
 	const md = new MarkdownIt().disable(['link', 'autolink', 'linkify']);
+
+	const adjustFontSize = async () => {
+		for (let i = 0; i < 10; i++) {
+			const sleepTime = 20 << i;
+			await sleep(sleepTime);
+			if (!thisEl) {
+				continue;
+			}
+
+			const documentRects = document.body.getClientRects();
+			const rects = thisEl.getClientRects();
+			if (!documentRects[0] || !rects[0]) {
+				continue;
+			}
+
+			const availWidth = documentRects[0].width;
+			const availHeight = window.screen.availHeight;
+
+			const aspectRatio = availWidth / availHeight;
+
+			const targetHeight = rects[0].width / aspectRatio;
+			const actualHeight = Math.min(availHeight, targetHeight);
+			shrink = availHeight - actualHeight;
+
+			const autoFontSize = (rects[0].width / BASE_WIDTH_PX) * BASE_FONT_SIZE_PERCENT;
+			actualFontSizePercent = autoFontSize;
+			paddingTopPx = actualHeight * 0.3;
+			return;
+		}
+	};
+
+	onMount(() => {
+		adjustFontSize();
+
+		window.addEventListener('resize', adjustFontSize);
+
+		return () => {
+			window.removeEventListener('resize', adjustFontSize);
+		};
+	});
 </script>
 
 <div
 	id={`slide-${slideId}`}
-	class={`markdown-body border-b p-16 ${slideId === 0 ? 'first-slide' : ''}`}
-	style={`height: calc(100vh - ${topGap}px);${fontSizeStyle}${titleSlideStyle}`}
+	bind:this={thisEl}
+	class={`markdown-body slide border-b ${slideId === 0 ? 'first-slide' : ''}`}
+	style={`height: calc(100vh - ${shrink}px);${fontSizeStyle}${titleSlideStyle}`}
 >
 	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
 	{@html md.render(slideMd)}
 </div>
 
 <style>
+	.slide {
+		padding: 3.5em;
+	}
 	.first-slide :global h1 {
 		font-size: 5em;
 	}
---
+++ src/routes/edit/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { getPresentationMd } from '$lib/stores/presentations';
+	import Presentation from '$lib/components/presentation.svelte';
+
+	let presentationId = $derived(page.url.searchParams.get('p'));
+	let presentationMd = $derived(await getPresentationMd(presentationId));
+</script>
+
+{#if presentationMd?.length > 0}
+	<Presentation {presentationMd} />
+{/if}
